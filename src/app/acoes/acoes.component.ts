723bcea6c91e2cf9403ac03e3b262162e41b481e
Lucas Alcazar <luccasalcazar@gmail.com>
2021-05-25 19:14:45 -0300

    atualizando github

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes'
+import { AcoesService } from './acoes.service'
+import { merge, Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent{
   acoesInput = new FormControl();
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    tap(() => { 
+      console.log('Fluxo do filtro');
+    }),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
-  constructor() {}
+  constructor(
+    private acoesService: AcoesService
+  ) {}
+
 }
